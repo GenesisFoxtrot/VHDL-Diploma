69d0c59074ee1bdbbdaf4dbb4019548f4d74eeb3
Valery Pyzhik <Genesis.trespas@gmail.com>
2016-04-15 18:34:51 +0300

    Missing Files

---
+++ Model/VHDLWords/Signals/FullSignal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Model.VHDLWords.Enumerations;
+
+namespace Model.VHDLWords.Signals
+{
+    public class FullSignal : Signal
+    {
+        public string ValueType { get; set; }
+        public string DefaultValue { get; set; }
+
+        public PartialSignal CreatePartial(EnumerationBase enumeration)
+        {
+            return new PartialSignal(this, Enumeration);
+        }
+
+        public string ToDefenitionVHDL()
+        {
+            return "  signal " + Name + " : " + ValueType + (" " + Enumeration ?? "") + ";\n";
+        }
+    }
+}
---
+++ Model/VHDLWords/Signals/PartialSignal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.VHDLWords.Enumerations;
+
+namespace Model.VHDLWords.Signals
+{
+    public class PartialSignal : Signal
+    {
+        public PartialSignal(FullSignal parentSignal, EnumerationBase enumeration)
+        {
+            Name = parentSignal.Name;
+            ParentSinal = parentSignal;
+            Enumeration = enumeration;
+        }
+
+        public FullSignal ParentSinal { get; }
+    }
+}
